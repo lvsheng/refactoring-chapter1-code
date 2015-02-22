5a7bf235bfa67e6c67c3afd25f3964029ee8be8a
lvsheng <lvsheng@baidu.com>
2015-02-22 17:25:38 +0800

    用多态取代frequentRenterPoint根据type的不同

--- src/Price.java
+++ src/Price.java
@@ -1,6 +1,11 @@
 abstract class Price {
     abstract int getPriceCode();
+
     abstract double getCharge (int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
 
 class ChildrensPrice extends Price {
@@ -8,7 +13,7 @@ class ChildrensPrice extends Price {
         return Movie.CHILDRENS;
     }
 
-    public double getCharge (int daysRented) {
+    double getCharge (int daysRented) {
         double result = 1.5;
         if (daysRented > 3)
             result += (daysRented - 3) * 1.5;
@@ -21,8 +26,12 @@ class NewReleasePrice extends Price {
         return Movie.NEW_RELEASE;
     }
 
-    public double getCharge (int daysRented) {
+    double getCharge (int daysRented) {
         return (double) (daysRented * 3);
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
     }
 }
 
@@ -31,7 +40,7 @@ class RegularPrice extends Price {
         return Movie.REGULAR;
     }
 
-    public double getCharge (int daysRented) {
+    double getCharge (int daysRented) {
         double result = 2;
         if (daysRented > 2)
             result += (daysRented - 2) * 1.5;
